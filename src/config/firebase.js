603486bc789bb5b11e285d6ce4fca312c8e0622f
Hrishi <nathh722@gmail.com>
2025-09-14 00:17:37 +0530

    zaz

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAdvH7B7Qxnde4n1ITNP-RChbdG-VFUW3I",
-  authDomain: "bloom-final-471317.firebaseapp.com",
-  projectId: "bloom-final-471317",
-  storageBucket: "bloom-final-471317.firebasestorage.app",
-  messagingSenderId: "577359325267",
-  appId: "1:577359325267:web:7e4d9d788104d34ccdbea6"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
